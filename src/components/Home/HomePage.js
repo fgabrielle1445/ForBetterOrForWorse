1d9cfb8dee079eddb4de71a732bf9f5b378132c8
Gabby <fgabrielle1445@yahoo.com>
2021-09-18 13:21:06 -0500

    Set default home page

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -60,77 +60,7 @@ export default function HomePage() {
                 <span class="v2_72">Within seconds, you will recieve a diagnosis with a high percentage of accuracy</span>
                 <span class="v2_73">Our dashboard will then recommend you referrals based on location and user ratings</span>
             </div>
-            <div class="v2_74">
-                <div class="name"></div>
-                <span class="v2_77">My Dashboard</span>
-                <span class="v2_81">Welcome User</span>
-                <div class="v2_100"></div>
-                <div class="v2_101"></div>
-                <span class="v2_102">U</span>
-                <div class="name"></div>
-                <div class="name"></div>
-                <div class="v2_2085"></div>
-            </div>
-            <div class="v2_2087">
-                <div class="name"></div>
-                <span class="v2_2092">My Dashboard</span>
-                <div class="v2_2093"></div>
-                <div class="v2_2094"></div>
-                <span class="v2_2097">Anxiety Diagnosistic Test</span>
-                <div class="v15_274"></div>
-                <div class="v15_251"></div>
-                <div class="v15_252">
-                    <div class="v15_253">
-                        <div class="v15_254">
-                            <div class="v15_255"></div>
-                        </div>
-                    </div>
-                    <div class="v15_256">
-                        <div class="v15_257">
-                            <div class="v15_258"></div>
-                        </div>
-                    </div>
-                </div>
-                <span class="v15_275">00:00:00</span>
-                <div class="v15_281"></div>
-                <div class="v15_282">
-                    <div class="v15_283">
-                        <div class="v15_284">
-                            <div class="v15_285"></div>
-                        </div>
-                    </div>
-                </div>
-                <span class="v15_301">Next Question</span>
-                <span class="v15_309">Record Answer</span>
-            </div>
-            <div class="v16_188">
-                <div class="name"></div>
-                <span class="v16_190">My Dashboard</span>
-                <div class="v16_191"></div>
-                <div class="v16_192"></div>
-                <span class="v16_195">Results</span>
-                <div class="v16_197"></div>
-                <span class="v16_198">Your anxiety diagnostic test revealed that you
-    are likely to have anxiety. This diagnosis is
-    not meant to be an official medical diagnosis 
-    but instead meant to provide you with more
-    insight to your mental health. 
-    
-    For additional questons, please feel free to look
-    at our FAQ section or contact the For Better
-    staff. Click here to learn more about anxiety.</span>
-            </div>
-            <div class="v2_74">
-                <div class="name"></div>
-                <span class="v2_77">My Dashboard</span>
-                <span class="v2_81">Welcome User</span>
-                <div class="v2_100"></div>
-                <div class="v2_101"></div>
-                <span class="v2_102">U</span>
-                <div class="name"></div>
-                <div class="name"></div>
-                <div class="v2_2085"></div>
-            </div>
+            
         </body>
     </html>
     
