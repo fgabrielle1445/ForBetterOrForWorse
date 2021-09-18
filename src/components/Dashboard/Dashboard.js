0e47aa6814e5e7a67263d45b2f5efc94a74bc604
Gabby <fgabrielle1445@yahoo.com>
2021-09-18 11:38:33 -0500

    Added files. Need to implement CSS

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import './Dashboard.css';
 
 export default function Dashboard() {
   return(
-    <h2>Dashboard</h2>
+    <html>
+      <head>
+        <link href="https://fonts.googleapis.com/css?family=Avenir&display=swap" rel="stylesheet" />
+        <link href="./Dashboard.css" rel="stylesheet" />
+        <title>Document</title>
+      </head>
+      <body>
+        <div class="v2_74"><div class="name"></div>
+        <span class="v2_77">My Dashboard</span>
+        <span class="v2_81">Welcome User</span>
+        <div class="v2_100"></div><div class="v2_101"></div>
+        <span class="v2_102">U</span><div class="name"></div>
+        <div class="name"></div><div class="v2_2085"></div></div>
+      </body>
+      </html>
   );
 }
