da6948f1c2652b8810f5ad37f09e40143994f9c6
istickman <idc3@rice.edu>
2021-09-18 23:25:42 -0500

    created link to results

--- src/components/AnxietyInterviewQ15/AnxietyInterview15.js
+++ src/components/AnxietyInterviewQ15/AnxietyInterview15.js
@@ -42,6 +42,7 @@ export default function AnxietyInterviewQ15() {
                 </div>
             </div>
             <span class="v15_301"><a href='https://tubbyelegantcomputationalscience--benearles.repl.co/'>Final Survey!</a></span>
+            <span class="v15_302"><a href='/results'>Results</a></span>
             <span class="v15_309">Record Answer</span>
         </div>
     </body>
