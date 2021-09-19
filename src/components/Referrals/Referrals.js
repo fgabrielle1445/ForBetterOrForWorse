a76a5a543995d16a5e4424775625b0c840038f96
istickman <idc3@rice.edu>
2021-09-18 23:48:57 -0500

    touched up referral page

--- src/components/Referrals/Referrals.js
+++ src/components/Referrals/Referrals.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Referrals.css';
 
 export default function Referrals() {
+  
   return(
     <html>
-      <h2>Ability to display available therapists in your area coming soon!</h2>
+      <head><link href="https://fonts.googleapis.com/css?family=Avenir&display=swap" rel="stylesheet" /><link href="./css/main.css" rel="stylesheet" /><title>Document</title></head><body><div class="v25_190"><div class="name"></div><span class="v25_192">My Dashboard</span><div class="v25_193"></div><div class="v25_194"></div><span class="v25_198">Find Nearerst Therapists</span><div class="v25_199"></div></div></body>
       </html>
   );
 }
