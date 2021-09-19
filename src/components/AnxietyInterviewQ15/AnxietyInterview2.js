d03a095c081b7e659efcb0d575e1f0143e0d958f
Gabby <fgabrielle1445@yahoo.com>
2021-09-18 22:36:36 -0500

    Adding questions

---
+++ src/components/AnxietyInterviewQ15/AnxietyInterview2.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './AnxietyInterview2.css';
+
+export default function AnxietyInterviewQ2() {
+  return(
+    <html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Avenir&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"/>
+        <link href="./css/main.css" rel="stylesheet"/>
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="v2_2087">
+            <div class="name"></div>
+            <span class="v2_2092"><a href="/dashboard">My Dashboard</a></span>
+            <div class="v2_2093"></div>
+            <div class="v2_2094"></div>
+            <span class="v2_2097">Anxiety Diagnosistic Test</span>
+            <div class="v15_274"></div>
+            <div class="v15_251"></div>
+            <div class="v15_252">
+                <div class="v15_253">
+                    <div class="v15_254">
+                        <div class="v15_255"></div>
+                    </div>
+                </div>
+                <div class="v15_256">
+                    <div class="v15_257">
+                        <div class="v15_258"></div>
+                    </div>
+                </div>
+            </div>
+            <span class="v15_275">00:00:00</span>
+            <div class="v15_281"></div>
+            <div class="v15_282">
+                <div class="v15_283">
+                    <div class="v15_284">
+                        <div class="v15_285"></div>
+                    </div>
+                </div>
+            </div>
+            <span class="v15_301">Next Question</span>
+            <span class="v15_309">Record Answer</span>
+        </div>
+    </body>
+    </html>
+
+  );
+}
